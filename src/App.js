cbc1e3a2fd579f19db0c2ea03116b3e153464483
Mher19971997 <mher.melqonyan.mher123@gmail.com>
2022-03-04 19:29:37 +0400

    Finished test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import ChuckNorris from './assets/images/ChuckNorris.png';
+import './style.css'
 
-function App() {
+const getLocalItems = () => {
+  let list = localStorage.getItem('lists');
+  if (list) {
+    return JSON.parse(localStorage.getItem('lists'))
+  } else {
+    return []
+  }
+}
+
+const App = () => {
+  const [data, setData] = useState(null)
+  const increment = useRef(null);
+  const [jokes, setJokes] = useState(getLocalItems())
+
+  const getJokes = async () => {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const movies = await response.json();
+    setData(movies)
+    return movies;
+  }
+
+  const handleStart = () => {
+    getJokes()
+    increment.current = setInterval(async () => {
+      getJokes()
+    }, 3000);
+  };
+
+  const handleReset = () => {
+    clearInterval(increment.current);
+  };
+
+  const addedList = () => {
+    if (jokes.includes(data?.value)) {
+      return deleteItem(jokes.indexOf((i) => i == data?.value));
+    }
+    if (jokes.length <= 10) {
+      return setJokes([...jokes, data?.value]);
+    }
+    let getListsArray = JSON.parse(localStorage.getItem('lists'))
+    if (a.length > 1) {
+      getListsArray.shift()
+      localStorage.removeItem('lists')
+      localStorage.setItem('lists', JSON.stringify(getListsArray))
+      setJokes(getListsArray)
+    }
+  };
+
+  const deleteItem = (index) => {
+    const updateitems = [...jokes]
+    updateitems.splice(index, 1)
+    setJokes(updateitems)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(jokes));
+  }, [jokes])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="imagesBlock">
+        <img src={ChuckNorris} />
+      </div>
+      <div className="jokesBlock">
+        <p>{data ? data?.value : 'No Jokes'}</p>
+      </div>
+      <div className="btnsGroup">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={addedList}>added</button>
+      </div>
+      <h2>Lists Jokes</h2>
+      <div className="listsBlock">
+        {jokes?.map((item, index) =>
+          <div key={index} className="listsItemBlock">
+            <p>{item}</p>
+            <button onClick={() => deleteItem(index)}>x</button>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
